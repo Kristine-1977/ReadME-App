bfe01bf18ca9fdcb5aa96e4ec5fa97e4b89c4f91
KJ Horton <kristinehrtn@yahoo.com>
2020-11-30 14:31:09 -0700

    commit all changes

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+// array of questions for user
+var inquirer = require('inquirer');
+const questions = [
+    {
+        type 'input',
+        name 'first-name',
+        message "What is your first name",
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
